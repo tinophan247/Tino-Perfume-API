9e8595512f8fcb972a4a9cd863e7d2515b2bcae3
Cao Hai Duong <46746869+haiduong004@users.noreply.github.com>
2023-06-26 01:32:15 +0700

    Revert "Update feature login API"

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const { sequelize } = require("./models");
-const { rootRouter } = require("./routes");
 
 app.use(express.json());
 
@@ -17,8 +16,6 @@ app.use(function(req, res, next) {
   res.setHeader("Access-Control-Allow-Credentials", true);
   next();
 });
-
-app.use("/api/v1",rootRouter);
 
 //lắng nghe sự kiện kết nối
 app.listen(5000, async () => {
