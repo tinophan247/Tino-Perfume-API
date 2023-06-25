3e81d2e8160cdf6636f0734322f918027de301aa
Tino Phan <tinophan247@gmail.com>
2023-06-25 22:35:04 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const { sequelize } = require("./models");
+
+app.use(express.json());
+
+//set up static file
+const publicPathDirectory = path.join(__dirname, "./public");
+app.use("/public", express.static(publicPathDirectory));
+
+app.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type, Accept,Authorization,Origin");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
+
+//lắng nghe sự kiện kết nối
+app.listen(5000, async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+});
